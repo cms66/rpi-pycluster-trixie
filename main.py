6ebabd8c1281e8d665a62b8a7e3216ef9bac89ae
cms66 <58144229+cms66@users.noreply.github.com>
2026-04-25 21:45:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import python.variables as pvar
 	
 def main():
 	pgen.read_config()
-	#pgen.show_config()
-	#phdw.setup_cam_csi()
+	#pgen.show_config() # Test function
+	#phdw.setup_cam_csi() # Works
 	#pgen.show_menu(pvar.mnuMainFull)
 
 if __name__ == "__main__":
