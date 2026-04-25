806dc9939b61428c1c93aee03da45e53a95293fa
cms66 <58144229+cms66@users.noreply.github.com>
2026-04-25 15:01:56 +0100

    Update variables.py

--- python/variables.py
+++ python/variables.py
@@ -1,1 +1,10 @@
+# Python variables
+import os
 
+hstname = str(os.uname()[1])
+usrname = os.getlogin() # Gives user with/without sudo
+# TODO get usrid from usrname 
+#usrid = os.getuid()
+#usrid = int(os.system("cat /etc/passwd | grep " + usrname + " cut -d ':' -f 3"))
+usrid = 1000
+arrconf = {}
