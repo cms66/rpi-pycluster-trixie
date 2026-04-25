85ad9eb3b4ee4b3e9955340f72475f445a3bcfd4
cms66 <58144229+cms66@users.noreply.github.com>
2026-04-25 15:02:03 +0100

    Update general.py

--- python/general.py
+++ python/general.py
@@ -2,3 +2,15 @@
 
 import os
 import python.variables as pvar
+
+def read_config():
+	conf = "/boot/firmware/custom.conf"
+	if os.path.exists(conf):
+		with open(conf) as f:
+			for line in f:
+				key = line.split('=')[0]
+				val = line.split('=')[1]
+				pvar.arrconf[key] = val
+
+def show_config():
+  
