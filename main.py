05fdb5f69f498a906e42497588dce6708a580975
cms66 <58144229+cms66@users.noreply.github.com>
2026-04-26 16:18:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # Python main entry point for setup + management
 
 import os
+import python.variables as pvar
 import python.general as pgen
 import python.hardware as phdw
-import python.variables as pvar
 import python.network as pnet
+import python.security as psec
 
 def main():
 	pgen.read_config()
