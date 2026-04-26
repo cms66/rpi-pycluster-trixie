7c6a7560a84f773396d299231700d8385364c51f
cms66 <58144229+cms66@users.noreply.github.com>
2026-04-26 12:44:15 +0100

    Update network.py

--- python/network.py
+++ python/network.py
@@ -6,8 +6,8 @@ import python.variables as pvar
 def create_user_ssh_keys():
 	# Create keys for user
 	#runuser -l  $usrname -c "ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -P \"\"" # Works including creates .ssh directory
-	#"sudo -u " + multipi + " ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -P ''"
-	os.system("sudo -u " + multipi + " ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -P ''")
+	#"sudo -u " + usrname + " ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -P ''"
+	os.system("sudo -u " + usrname + " ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -P ''")
 	strcmd = "echo 'HostKey /home/" + usrname + "/.ssh/id_ed25519' >> /etc/ssh/sshd_config"
 	os.system(strcmd)
 	os.system("systemctl restart ssh")
@@ -15,7 +15,7 @@ def create_user_ssh_keys():
 
 def copy_user_ssh_keys():
 	remnode = input("Remote node")
-	os.system("sudo -u " + multipi + " ssh-copy-id -i /home/" + usrname + "/.ssh/id_ed25519 " + usrname + "@" + remnode)
+	os.system("sudo -u " + usrname + " ssh-copy-id -i /home/" + usrname + "/.ssh/id_ed25519 " + usrname + "@" + remnode)
 	input("SSH key copied - press enter to continue")
 
 def delete_node_from_known_hosts():
