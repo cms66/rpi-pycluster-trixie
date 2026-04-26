d9e26e6b6a13e103a6a1026b2844f4ddb8068cda
cms66 <58144229+cms66@users.noreply.github.com>
2026-04-26 11:47:32 +0100

    Create network.py

---
+++ python/network.py
@@ -0,0 +1,21 @@
+# Network functions
+
+import python.variables as pvar
+
+def create_user_ssh_keys():
+	input("TODO - press enter to continue")
+
+def copy_user_ssh_keys():
+	input("TODO - press enter to continue")
+
+def delete_node_from_known_hosts():
+	input("TODO - press enter to continue")
+
+def install_nfs_server():
+	input("TODO - press enter to continue")
+
+def add_nfs_local():
+	input("TODO - press enter to continue")
+
+def add_nfs_remote():
+	input("TODO - press enter to continue")
