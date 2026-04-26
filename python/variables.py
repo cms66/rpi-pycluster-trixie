a798b20fed11d8b2a8a3b382249ea9ab5a2f8dc4
cms66 <58144229+cms66@users.noreply.github.com>
2026-04-26 08:59:29 +0100

    Update variables.py

--- python/variables.py
+++ python/variables.py
@@ -1,12 +1,10 @@
 # Python variables
 import os
+from pwd import getpwnam
 
 hstname = str(os.uname()[1])
 usrname = os.getlogin() # Gives user with/without sudo
-# TODO get usrid from usrname 
-#usrid = os.getuid()
-usrid = int(os.system("cat /etc/passwd | grep " + usrname + " | cut -d ':' -f 3"))
-#usrid = 1000
+usrid = getpwnam(usrname).pw_uid
 arrconf = {}
 
 # Menus
