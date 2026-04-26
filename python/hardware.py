1a88d6161ad5c716c112b4bac34d10126bed6f98
cms66 <58144229+cms66@users.noreply.github.com>
2026-04-26 09:30:15 +0100

    Update hardware.py

--- python/hardware.py
+++ python/hardware.py
@@ -3,7 +3,7 @@
 import python.variables as pvar
 
 def setup_cam_csi():
-	read -p "CSI Cam = TODO - press enter to continue"
+	input("CSI Cam = TODO - press enter to continue")
 
 def setup_pcie():
-	read -p "PCIE = TODO - press enter to continue"
+	input("PCIE = TODO - press enter to continue")
