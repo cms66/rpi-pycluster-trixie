fb66cb6cbd5fadc003ac761e1b0220d59a434fc2
cms66 <58144229+cms66@users.noreply.github.com>
2026-04-26 13:03:55 +0100

    Update network.py

--- python/network.py
+++ python/network.py
@@ -5,21 +5,20 @@ import python.variables as pvar
 
 def create_user_ssh_keys():
 	# Create keys for user
-	#runuser -l  $usrname -c "ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -P \"\"" # Works including creates .ssh directory
-	#"sudo -u " + usrname + " ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -P ''"
 	os.system("sudo -u " + pvar.usrname + " ssh-keygen -t ed25519 -f /home/" + pvar.usrname + "/.ssh/id_ed25519 -P ''")
-	strcmd = "echo 'HostKey /home/" + pvar.usrname + "/.ssh/id_ed25519' >> /etc/ssh/sshd_config"
-	os.system(strcmd)
+	os.system("echo 'HostKey /home/" + pvar.usrname + "/.ssh/id_ed25519' >> /etc/ssh/sshd_config")
 	os.system("systemctl restart ssh")
 	input("SSH key setup done - press enter to continue")
 
 def copy_user_ssh_keys():
-	remnode = input("Remote node")
+	remnode = input("Remote node: ")
 	os.system("sudo -u " + pvar.usrname + " ssh-copy-id -i /home/" + pvar.usrname + "/.ssh/id_ed25519 " + pvar.usrname + "@" + remnode)
-	input("SSH key copied - press enter to continue")
+	input("SSH key copied to " + remnode + " - press enter to continue")
 
 def delete_node_from_known_hosts():
-	input("TODO - press enter to continue")
+	remnode = input("Remote node: ")
+	os.system("sudo -u " + pvar.usrname + " ssh-keygen -f /home/" + pvar.usrname + "/.ssh/known_hosts -R " + remnode
+	input(remnode + " removed from known hosts - press enter to continue")
 
 def install_nfs_server():
 	input("TODO - press enter to continue")
