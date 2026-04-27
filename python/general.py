eca8331fe6636d82c0e6b47d36f6bbc9b75d596a
cms66 <58144229+cms66@users.noreply.github.com>
2026-04-27 13:04:51 +0100

    Update general.py

--- python/general.py
+++ python/general.py
@@ -56,6 +56,19 @@ def show_system_summary():
 	os.system("ufw status")
 	input("Press enter to continue")
 
+def install_modules_server():
+    os.system("apt-get -y install tcl tcl-dev m4 sphinx autoconf automake autopoint")
+    os.system("git clone https://github.com/envmodules/modules.git")
+    os.chdir("modules")
+    os.system("./configure --prefix=/usr/local")
+    os.system("make; make install; ldconfig")
+	os.chdir("..")
+    # Modules initialization
+	with open('.bashrc', 'a') as f:
+		f.write("'. /usr/local/init/bash'")   
+	os.system("rm -rf modules*")
+    read -p "Environment Modules install done, press enter to continue" input
+}
 def show_menu(menu):
 	prompt = "Select option: "
 	while True:
