9be6c05c61cbc5ccafd5da85f6ea3da446db1dce
cms66 <58144229+cms66@users.noreply.github.com>
2026-04-27 13:39:05 +0100

    Update general.py

--- python/general.py
+++ python/general.py
@@ -5,6 +5,7 @@ import python.variables as pvar
 import python.hardware as phdw
 import python.network as pnet
 import python.security as psec
+import python.envmod as pmod
 
 def read_config():
 	conf = "/boot/firmware/custom.conf"
@@ -55,25 +56,6 @@ def show_system_summary():
 	os.system("nmcli dev status")
 	os.system("ufw status")
 	input("Press enter to continue")
-
-def install_modules_server():
-	os.system("apt-get -y install tcl tcl-dev m4 sphinx autoconf automake autopoint")
-	os.system("git clone https://github.com/envmodules/modules.git")
-	os.chdir("modules")
-	os.system("./configure --prefix=/usr/local")
-	os.system("make; make install; ldconfig")
-	os.chdir("..")
-	# Modules initialization
-	with open('.bashrc', 'a') as f:
-		f.write(". /usr/local/init/bash")
-	os.system("rm -rf modules*")
-	input("Environment Modules server install done, press enter to continue")
-
-def install_modules_client():
-	os.system("apt-get -y install tcl")
-	with open('.bashrc', 'a') as f:
-		f.write(". /usr/local/init/bash")
-	input("Environment Modules client install done, press enter to continue")
 
 def show_menu(menu):
 	prompt = "Select option: "
