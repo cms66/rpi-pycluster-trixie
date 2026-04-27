4f26ba67fd535f1bc04b91fad0c2ef3fc35aae6e
cms66 <58144229+cms66@users.noreply.github.com>
2026-04-27 11:20:45 +0100

    Update hardware.py

--- python/hardware.py
+++ python/hardware.py
@@ -5,7 +5,7 @@ import python.variables as pvar
 def setup_pcie():
 	# TODO - Check Model
 	# Enable Gen 3 - Check for Pi 5/CM5
-	with open("/boot/firmware/config.txt", a) as f:
+	with open("/boot/firmware/config.txt", "a") as f:
 		content = f.read()
 		if "dtparam=pciex1_gen=3" in content:
 			input("PCIe Gen 3 already enabled, press enter to continue")
