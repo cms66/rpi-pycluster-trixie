802f72011579b9248d17eb12f88f8a04ecb5f73b
cms66 <58144229+cms66@users.noreply.github.com>
2026-04-27 16:41:01 +0100

    Update envmod.py

--- python/envmod.py
+++ python/envmod.py
@@ -1,14 +1,15 @@
 # Environment Modules funcions
 
 import os
+import python.variables as pvar
 
 def install_modules_server():
 	os.chdir(usrpath)
-	#os.system("apt-get -y install tcl tcl-dev m4 sphinx autoconf automake autopoint")
-	#os.system("git clone https://github.com/envmodules/modules.git")
-	#os.chdir("modules")
-	#os.system("./configure --prefix=/usr/local")
-	#os.system("make; make install; ldconfig")
+	os.system("apt-get -y install tcl tcl-dev m4 sphinx autoconf automake autopoint")
+	os.system("git clone https://github.com/envmodules/modules.git")
+	os.chdir("modules")
+	os.system("./configure --prefix=/usr/local")
+	os.system("make; make install; ldconfig")
 	os.chdir(usrpath)
 	# Modules initialization
 	with open('.bashrc', 'a') as f:
