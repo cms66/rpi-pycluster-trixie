bcb684d7f0df7c6b334a956be452dc853f28d264
cms66 <58144229+cms66@users.noreply.github.com>
2026-04-27 11:30:06 +0100

    Update hardware.py

--- python/hardware.py
+++ python/hardware.py
@@ -5,15 +5,16 @@ import python.variables as pvar
 def setup_pcie():
 	# TODO - Check Model
 	# Enable Gen 3 - Check for Pi 5/CM5
-	with open("/boot/firmware/config.txt", "a") as f:
-		content = f.read()
-		if "dtparam=pciex1_gen=3" in content:
-			input("PCIe Gen 3 already enabled, press enter to continue")
-		else:
-			f.write("dtparam=pciex1")
-			f.write("dtparam=pciex1_gen=3")
-			input("PCIe Gen 3 enabled (reboot required), press enter to continue")
-	return
+	#with open("/boot/firmware/config.txt", "a") as f:
+	#	content = f.read()
+	#	if "dtparam=pciex1_gen=3" in content:
+	#		input("PCIe Gen 3 already enabled, press enter to continue")
+	#	else:
+	#		f.write("dtparam=pciex1")
+	#		f.write("dtparam=pciex1_gen=3")
+	#		input("PCIe Gen 3 enabled (reboot required), press enter to continue")
+	#return
+	input("PCIe = TODO - press enter to continue")
 	
 def setup_cam_csi():
 	input("CSI Cam = TODO - press enter to continue")
