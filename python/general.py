a931a7066be8a52e00c13b6624d0f96d06388308
cms66 <58144229+cms66@users.noreply.github.com>
2026-04-27 13:26:33 +0100

    Update general.py

--- python/general.py
+++ python/general.py
@@ -65,9 +65,17 @@ def install_modules_server():
 	os.chdir("..")
 	# Modules initialization
 	with open('.bashrc', 'a') as f:
-		f.write("'. /usr/local/init/bash'")
+		f.write(". /usr/local/init/bash")
 	os.system("rm -rf modules*")
-	input("Environment Modules install done, press enter to continue")
+	input("Environment Modules server install done, press enter to continue")
+
+install_modules_client()
+{
+    apt-get -y install tcl
+	os.system("apt-get -y install tcl")
+	with open('.bashrc', 'a') as f:
+		f.write(". /usr/local/init/bash")
+	input("Environment Modules client install done, press enter to continue")
 
 def show_menu(menu):
 	prompt = "Select option: "
