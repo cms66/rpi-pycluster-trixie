ce093b68b855076f6d767454bf4c66a9d32204e6
cms66 <58144229+cms66@users.noreply.github.com>
2026-04-27 11:28:03 +0100

    Update hardware.py

--- python/hardware.py
+++ python/hardware.py
@@ -7,12 +7,13 @@ def setup_pcie():
 	# Enable Gen 3 - Check for Pi 5/CM5
 	with open("/boot/firmware/config.txt", "a") as f:
 		content = f.read()
-		if dtparam=pciex1_gen=3 in content:
+		if "dtparam=pciex1_gen=3" in content:
 			input("PCIe Gen 3 already enabled, press enter to continue")
 		else:
 			f.write("dtparam=pciex1")
 			f.write("dtparam=pciex1_gen=3")
 			input("PCIe Gen 3 enabled (reboot required), press enter to continue")
+	return
 	
 def setup_cam_csi():
 	input("CSI Cam = TODO - press enter to continue")
