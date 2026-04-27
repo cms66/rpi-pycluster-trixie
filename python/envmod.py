3103feb62091d5497c87672408874290d6e1a0e1
cms66 <58144229+cms66@users.noreply.github.com>
2026-04-27 13:36:14 +0100

    Create envmod.py

---
+++ python/envmod.py
@@ -0,0 +1,22 @@
+# Environment Modules funcions
+
+import os
+
+def install_modules_server():
+	os.system("apt-get -y install tcl tcl-dev m4 sphinx autoconf automake autopoint")
+	os.system("git clone https://github.com/envmodules/modules.git")
+	os.chdir("modules")
+	os.system("./configure --prefix=/usr/local")
+	os.system("make; make install; ldconfig")
+	os.chdir("..")
+	# Modules initialization
+	with open('.bashrc', 'a') as f:
+		f.write(". /usr/local/init/bash")
+	os.system("rm -rf modules*")
+	input("Environment Modules server install done, press enter to continue")
+
+def install_modules_client():
+	os.system("apt-get -y install tcl")
+	with open('.bashrc', 'a') as f:
+		f.write(". /usr/local/init/bash")
+	input("Environment Modules client install done, press enter to continue")
