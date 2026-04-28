4927a2c6b1ed8a4137a03a7bc8df1f2336d36315
cms66 <58144229+cms66@users.noreply.github.com>
2026-04-28 17:33:05 +0100

    Update security.py

--- python/security.py
+++ python/security.py
@@ -1,9 +1,17 @@
 # Security functions
 
 import os
+import python.variables as pvar
+
+def test_func():
+	print(pvar.usrname)
+	print(pvar.usrid)
+	print(pvar.usrpath)
+	print(pvar.hstname)
+	print(pvar.arrconf[gitrepo])
 
 # fail2ban
 
 # ufw
 def get_ufw_status():
-  os.system("ufw status")
+	os.system("ufw status")
