e8a0577c75889aaf2cc81bad454208663213991c
cms66 <58144229+cms66@users.noreply.github.com>
2026-09-20 15:25:20 +0100

    Update security.py

--- python/security.py
+++ python/security.py
@@ -4,15 +4,6 @@ import os
 import python.variables as pvar
 import python.general as pgen
 
-def test_func():
-	pgen.read_config()
-	print(pvar.usrname)
-	print(pvar.usrid)
-	print(pvar.usrpath)
-	print(pvar.hstname)
-	print(pvar.arrconf['gitrepo'])
-	input("Continue")
-
 # fail2ban
 
 # ufw
